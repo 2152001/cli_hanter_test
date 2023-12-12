d8c1295173e521b328e88322b7d523339fd8427e
anas12312 <anashesham000@gmail.com>
2023-12-12 11:41:56 +0200

    anas

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rules: {
+        no_eval: "error",
+        no_like_eval: "error",
+        no_buffer_constructor: "error",
+        no_prototype_builtins: "warning",
+        prefer_const: "warning"
+    }
+}
